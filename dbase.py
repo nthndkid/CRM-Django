312107263f1872512947c624d0ec60917681fa2f
nthndkid <143003322+nthndkid@users.noreply.github.com>
2024-10-24 14:07:18 +0800

    first commit

---
+++ dbase.py
@@ -0,0 +1,19 @@
+# pip install my-sql-connector
+# pip install my-sql-connector-python
+
+import mysql.connector
+
+# database connection
+dbase = mysql.connector.connect(
+    host="localhost",
+    user = 'root',
+    password = 'floraphael13',
+)
+
+# prepare a cursor object
+cursorObj = dbase.cursor()
+
+# create a database
+cursorObj.execute("CREATE DATABASE crm_dbase")
+
+print("Done!")
